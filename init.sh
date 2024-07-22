4f7da84a911f847a88fb9349918a2bcbc653af8c
Maciek Mazurek <maciek.mazurek@protonmail.com>
2024-07-22 22:07:49 +0200

    fixed latex problem: :

--- init.sh
+++ init.sh
@@ -5,6 +5,8 @@ sudo apt-get install software-properties-common -y
 sudo apt-get install python3.12
 sudo apt-get install python3.12-distutils
 sudo apt-get install curl
+sudo apt-get install texlive-science
+sudo apt-get install texlive-latex-extra
 sudo apt-get install emacs
 sudo apt-get install fonts-firacode
 sudo apt-get install elpa-counsel
